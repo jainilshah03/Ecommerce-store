ffc17fdc1034c34bf458c45c2a98b698c93b5a13
jainil shah <97247891+jainilshah03@users.noreply.github.com>
2025-06-12 19:36:51 +0530

    console2

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,8 +3,8 @@ import { Category } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-    const res = await fetch(URL);
-
+    const res = await fetch(URL, {cache: "no-store"});
+    console.log("Fetched categories:", res);
     return res.json(); // only parse if it's valid JSON
 };
 
