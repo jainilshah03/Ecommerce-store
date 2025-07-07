465dbddc1920c47fbe2a7ea8aba1c8e93419d3c0
jainil shah <97247891+jainilshah03@users.noreply.github.com>
2025-07-07 19:15:05 +0530

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-white border-t border-gray-200">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    @2023 FakeStoreNameA, Inc. All rights reserved.
+                    @2025 TestStoreA, Inc. All rights reserved.
                 </p>
             </div>
         </footer>
